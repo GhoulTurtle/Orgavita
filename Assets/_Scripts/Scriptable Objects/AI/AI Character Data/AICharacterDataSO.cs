3bfbeff649049c92c2f21fbac16c22dce7c49547
CalebRichardson1 <crichnumber2@gmail.com>
2024-07-31 21:41:59 -0500

    Started AI Transitions Part 1

--- Assets/_Scripts/Scriptable Objects/AI/AI Character Data/AICharacterDataSO.cs
+++ Assets/_Scripts/Scriptable Objects/AI/AI Character Data/AICharacterDataSO.cs
@@ -17,6 +17,8 @@ public class AICharacterDataSO : ScriptableObject{
     [Header("Base Timer Variables")]
     [Tooltip("The total amount of time that the AI will search for a target in a area.")]
     [Range(5f, 30f)] public float looseTimeInSeconds;
+    [Tooltip("The total amount of time that the AI will take to spot a target with LOS.")]
+    [Range(0.5f, 3.5f)] public float spotTimeInSeconds;
     [Tooltip("The amount of time that the AI will be idle.")]
     [MinMaxRange(1f, 600f)] public RangedFloat idleTimeInSeconds;
     [Tooltip("The amount of time that the AI will keep track of the player while the player isn't in the LOS while attacking.")]
